8b8eb163e8a6b4bb3f65cb5dfd267afc26924838
yota-p <yh@dlvm-1.asia-east1-b.c.local-abbey-244223.internal>
2019-08-25 07:38:07 +0000

    created setup shellscript

---
+++ setup.sh
@@ -0,0 +1,22 @@
+# Property file
+cp .bashrc ~
+cp .emacs ~
+source ~/.bashrc
+
+# Emacs
+cd ~
+sudo apt-get install ncurses-dev
+wget https://ftp.gnu.org/gnu/emacs/emacs-26.2.tar.gz
+tar zvxf emacs-26.2.tar.gz
+cd emacs-26.2
+./configure --with-gnutls=no
+make
+sudo make install
+rm ~/emacs-26.2.tar.gz
+rm -r ~/emacs-26.2.tar.gz
+
+# CookieCutter
+pip3 install cookiecutter
+
+# Watermark
+pip3 install watermark
