fbac395bf3db9b6317cdf95eabbf02e5f14e50d2
root <root@host>
2019-10-19 07:58:59 +0000

    mod .bashrc

--- setup.sh
+++ setup.sh
@@ -4,19 +4,7 @@
 cp .bashrc ~
 cp .emacs ~
 
-# Emacs 1
-#cd ~
-#sudo apt-get install ncurses-dev
-#wget https://ftp.gnu.org/gnu/emacs/emacs-26.2.tar.gz
-#tar zvxf emacs-26.2.tar.gz
-#cd emacs-26.2
-#./configure --with-gnutls=no
-#make
-#sudo make install
-#rm ~/emacs-26.2.tar.gz
-#rm -r ~/emacs-26.2
-
-# Emacs 2
+# Emacs
 apt-get install software-properties-common
 apt-get update
 add-apt-repository ppa:kelleyk/emacs
