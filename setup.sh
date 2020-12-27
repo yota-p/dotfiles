7959c0acfee0b5668c8b69fe3a0d1d0cc3a3807f
yota-p <930713yh@gmail.com>
2020-12-27 05:47:57 +0000

    updated

--- setup.sh
+++ setup.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
+# Bash
+cp .bashrc ~
+source ~/.bashrc
+
+# apt
+apt update
+apt upgrade
+
+# Emacs
+#apt-get install software-properties-common
+add-apt-repository ppa:kelleyk/emacs
+apt update
+apt install emacs26
+cp init.el ~/.emacs.d/
+
+# tmux
+apt-get install tmux
+cp .tmux.conf ~
+
 # Update pip
 pip install --upgrade pip
 
 # Kaggle-API
 pip install kaggle
 
-# Emacs
-apt-get install software-properties-common
-apt-get update
-add-apt-repository ppa:kelleyk/emacs
-apt-get update
-apt-get install emacs25
-cp init.el ~/.emacs.d/
-
-# Bash
-cp .bashrc ~
-source ~/.bashrc
-
-# tmux
-apt-get install tmux
-cp .tmux.conf ~
-
 # ls_colors
-mkdir ~/.dircolors
-wget -P ~/.dircolors https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.256dark
+#mkdir ~/.dircolors
+#wget -P ~/.dircolors https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.256dark
